f0a80e846bd44e378fd231a31ab46c08e3e79f6e
David Garcia.M <djgarcia.24@est.ucab.edu.ve>
2026-07-09 20:43:55 -0400

    inicio completo sin spinner

--- js/inicio_sesion.js
+++ js/inicio_sesion.js
@@ -1,14 +1,34 @@
 const URL_SERVIDOR = "https://servidor-proyecto-web-2.onrender.com";
 
-async function obtenerUsuario() {
+
+async function iniciarSesion(emailUsuario, passwordUsuario) {
     try {
-        const respuesta = await fetch(`${URL_SERVIDOR}/usuario-uno`);
+        // Configuramos el fetch para que mande un paquete POST cerrado
+        const respuesta = await fetch(`${URL_SERVIDOR}/login`, {
+            method: "POST", 
+            headers: {
+                "Content-Type": "application/json" // Le avisamos al servidor que va un JSON adentro
+            },
+            body: JSON.stringify({ 
+                email: emailUsuario, 
+                password: passwordUsuario 
+            }) // Metemos los datos dentro del remolque
+        });
+
         const datos = await respuesta.json();
-        
-        // ¡Aquí lo imprimes en tu consola!
-        console.log("El usuario recibido es:", datos);
+
+        if (!respuesta.ok) {
+            // Si el servidor responde con 401 (error), lanzamos el mensaje aquí
+            throw new Error(datos.error);
+        }
+
+        console.log("Servidor dice:", datos.mensaje);
+        alert("¡Entraste con éxito!");
+        return true
+
     } catch (error) {
-        console.error("Hubo un error al conectar:", error);
+        console.error("Error en el login:", error.message);
+        alert("Error: " + error.message);
     }
 }
 
@@ -22,8 +42,15 @@ const boton = document.getElementById('iniciar_sesion');
 
 boton.addEventListener('click', function(event) {
     event.preventDefault(); 
-    
-    // Simplemente llamamos a la función
-    obtenerUsuario();
+
+    const email = document.getElementById('email_text').value;
+    const  password = document.getElementById('password_text').value;
+    console.log(email, password);
+
+
+    //ZONA DE SPINNER
+    if (iniciarSesion(email, password) ){
+       window.location.href = './html/buscador.html';
+    }
 
 })
